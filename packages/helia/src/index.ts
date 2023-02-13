5d72f4fb3fa5ecf44537db0d2f8c6124888551f4
achingbrain <alex@achingbrain.net>
2023-02-13 12:34:47 +0100

    chore: add custom hashers

--- packages/helia/src/index.ts
+++ packages/helia/src/index.ts
@@ -6,12 +6,22 @@
  * @example
  *
  * ```typescript
+ * import { createLibp2p } from 'libp2p'
+ * import { MemoryDatastore } from 'datastore-core'
+ * import { MemoryBlockstore } from 'blockstore-core'
  * import { createHelia } from 'helia'
+ * import { unixfs } from '@helia/unixfs'
  * import { CID } from 'multiformats/cid'
  *
- * const node = await createHelia()
- *
- * node.cat(CID.parse('bafyFoo'))
+ * const node = await createHelia({
+ *   blockstore: new MemoryBlockstore(),
+ *   datastore: new MemoryDatastore(),
+ *   libp2p: await createLibp2p({
+ *     //... libp2p options
+ *   })
+ * })
+ * const fs = unixfs(node)
+ * fs.cat(CID.parse('bafyFoo'))
  * ```
  */
 
@@ -23,6 +33,9 @@ import type { Libp2p } from '@libp2p/interface-libp2p'
 import type { Blockstore } from 'interface-blockstore'
 import type { AbortOptions } from '@libp2p/interfaces'
 import type { Datastore } from 'interface-datastore'
+import type { MultihashHasher } from 'multiformats/hashes/interface'
+import { sha256, sha512 } from 'multiformats/hashes/sha2'
+import { identity } from 'multiformats/hashes/identity'
 
 export interface CatOptions extends AbortOptions {
   offset?: number
@@ -53,17 +66,40 @@ export interface HeliaInit {
    * The datastore is where data is stored
    */
   datastore: Datastore
+
+  /**
+   * By default sha256, sha512 and identity hashes are supported for
+   * bitswap operations. To bitswap blocks with CIDs using other hashes
+   * pass appropriate MultihashHashers here.
+   */
+  hashers?: MultihashHasher[]
 }
 
 /**
- * Create and return a Helia node.
- *
- * @param {HeliaInit} init
- * @returns {Promise<Helia>}
+ * Create and return a Helia node
  */
 export async function createHelia (init: HeliaInit): Promise<Helia> {
-  const bitswap = createBitswap(init.libp2p, init.blockstore, {
+  const hashers: MultihashHasher[] = [
+    sha256,
+    sha512,
+    identity,
+    ...(init.hashers ?? [])
+  ]
 
+  const bitswap = createBitswap(init.libp2p, init.blockstore, {
+    hashLoader: {
+      getHasher: async (codecOrName: string | number) => {
+        const hasher = hashers.find(hasher => {
+          return hasher.code === codecOrName || hasher.name === codecOrName
+        })
+
+        if (hasher != null) {
+          return await Promise.resolve(hasher)
+        }
+
+        throw new Error(`Could not load hasher for code/name "${codecOrName}"`)
+      }
+    }
   })
   bitswap.start()
 
