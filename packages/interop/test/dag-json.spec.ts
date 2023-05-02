78d2e407c2e4ca110d1aa44c1fb7aa554200ebf9
achingbrain <alex@achingbrain.net>
2023-05-02 18:31:20 +0100

    chore: rename variable

--- packages/interop/test/dag-json.spec.ts
+++ packages/interop/test/dag-json.spec.ts
@@ -2,7 +2,7 @@
 
 import { dagJson, type DAGJSON, type AddOptions } from '@helia/dag-json'
 import { expect } from 'aegir/chai'
-import * as jsonCodec from 'multiformats/codecs/json'
+import * as codec from 'multiformats/codecs/json'
 import { createHeliaNode } from './fixtures/create-helia.js'
 import { createKuboNode } from './fixtures/create-kubo.js'
 import type { Helia } from '@helia/interface'
@@ -16,7 +16,7 @@ describe('dag-json interop', () => {
 
   async function expectSameCid (data: () => any, heliaOpts: Partial<AddOptions> = {}, kuboOpts: KuboAddOptions = { format: 'dag-json' }): Promise<void> {
     const heliaCid = await d.add(data(), heliaOpts)
-    const kuboCid = await kubo.api.block.put(jsonCodec.encode(data()), kuboOpts)
+    const kuboCid = await kubo.api.block.put(codec.encode(data()), kuboOpts)
 
     expect(heliaCid.toString()).to.equal(kuboCid.toString())
   }
@@ -49,14 +49,14 @@ describe('dag-json interop', () => {
     const input = { hello: 'world' }
     const cid = await d.add(input)
     const block = await kubo.api.block.get(cid)
-    const output = jsonCodec.decode(block)
+    const output = codec.decode(block)
 
     expect(output).to.deep.equal(input)
   })
 
   it('should add to kubo and fetch from helia', async () => {
     const input = { hello: 'world' }
-    const cid = await kubo.api.block.put(jsonCodec.encode(input))
+    const cid = await kubo.api.block.put(codec.encode(input))
     const output = await d.get(cid)
 
     expect(output).to.deep.equal(input)
