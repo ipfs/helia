95e68a12ac7f829b7aa455b571f942dfc82394ed
achingbrain <alex@achingbrain.net>
2023-06-07 10:50:57 +0100

    feat: initial import

---
+++ packages/car/README.md
@@ -0,0 +1,59 @@
+<p align="center">
+  <a href="https://github.com/ipfs/helia" title="Helia">
+    <img src="https://raw.githubusercontent.com/ipfs/helia/main/assets/helia.png" alt="Helia logo" width="300" />
+  </a>
+</p>
+
+# @helia/car <!-- omit in toc -->
+
+[![ipfs.tech](https://img.shields.io/badge/project-IPFS-blue.svg?style=flat-square)](https://ipfs.tech)
+[![Discuss](https://img.shields.io/discourse/https/discuss.ipfs.tech/posts.svg?style=flat-square)](https://discuss.ipfs.tech)
+[![codecov](https://img.shields.io/codecov/c/github/ipfs/helia-car.svg?style=flat-square)](https://codecov.io/gh/ipfs/helia-car)
+[![CI](https://img.shields.io/github/actions/workflow/status/ipfs/helia-car/js-test-and-release.yml?branch=main\&style=flat-square)](https://github.com/ipfs/helia-car/actions/workflows/js-test-and-release.yml?query=branch%3Amain)
+
+> Import/export car files from Helia
+
+## Table of contents <!-- omit in toc -->
+
+- [Install](#install)
+  - [Browser `<script>` tag](#browser-script-tag)
+- [API Docs](#api-docs)
+- [License](#license)
+- [Contribute](#contribute)
+
+## Install
+
+```console
+$ npm i @helia/car
+```
+
+### Browser `<script>` tag
+
+Loading this module through a script tag will make it's exports available as `HeliaCar` in the global namespace.
+
+```html
+<script src="https://unpkg.com/@helia/car/dist/index.min.js"></script>
+```
+
+## API Docs
+
+- <https://ipfs.github.io/helia-car/modules/_helia_car.html>
+
+## License
+
+Licensed under either of
+
+- Apache 2.0, ([LICENSE-APACHE](LICENSE-APACHE) / <http://www.apache.org/licenses/LICENSE-2.0>)
+- MIT ([LICENSE-MIT](LICENSE-MIT) / <http://opensource.org/licenses/MIT>)
+
+## Contribute
+
+Contributions welcome! Please check out [the issues](https://github.com/ipfs/helia-car/issues).
+
+Also see our [contributing document](https://github.com/ipfs/community/blob/master/CONTRIBUTING_JS.md) for more information on how we work, and about contributing in general.
+
+Please be aware that all interactions related to this repo are subject to the IPFS [Code of Conduct](https://github.com/ipfs/community/blob/master/code-of-conduct.md).
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
+
+[![](https://cdn.rawgit.com/jbenet/contribute-ipfs-gif/master/img/contribute.gif)](https://github.com/ipfs/community/blob/master/CONTRIBUTING.md)
---
+++ packages/interop/test/index.spec.ts
@@ -0,0 +1,96 @@
+/* eslint-env mocha */
+
+import { car } from '@helia/car'
+import { type UnixFS, unixfs } from '@helia/unixfs'
+import { CarReader } from '@ipld/car'
+import { expect } from 'aegir/chai'
+import toBuffer from 'it-to-buffer'
+import { createHeliaNode } from './fixtures/create-helia.js'
+import { createKuboNode } from './fixtures/create-kubo.js'
+import { memoryCarWriter } from './fixtures/memory-car.js'
+import type { Car } from '@helia/car'
+import type { Helia } from '@helia/interface'
+import type { FileCandidate } from 'ipfs-unixfs-importer'
+import type { Controller } from 'ipfsd-ctl'
+
+describe('car interop', () => {
+  let helia: Helia
+  let c: Car
+  let u: UnixFS
+  let kubo: Controller
+
+  beforeEach(async () => {
+    helia = await createHeliaNode()
+    c = car(helia)
+    u = unixfs(helia)
+    kubo = await createKuboNode()
+
+    // connect helia to kubo
+    await helia.libp2p.dial(kubo.peer.addresses)
+  })
+
+  afterEach(async () => {
+    if (helia != null) {
+      await helia.stop()
+    }
+
+    if (kubo != null) {
+      await kubo.stop()
+    }
+  })
+
+  it('should export a car from Helia, import and read the contents from kubo', async () => {
+    const chunkSize = 1024 * 1024
+    const size = chunkSize * 10
+    const input: Uint8Array[] = []
+
+    const candidate: FileCandidate = {
+      content: (async function * () {
+        for (let i = 0; i < size; i += chunkSize) {
+          const buf = new Uint8Array(chunkSize)
+          input.push(buf)
+
+          yield buf
+        }
+      }())
+    }
+
+    const cid = await u.addFile(candidate)
+    const writer = memoryCarWriter(cid)
+    await c.export(cid, writer)
+
+    const buf = await writer.bytes()
+
+    kubo.api.dag.import([buf])
+
+    const bytes = await toBuffer(kubo.api.cat(cid))
+
+    expect(bytes).to.equalBytes(toBuffer(input))
+  })
+
+  it('should export a car from kubo, import and read the contents from Helia', async () => {
+    const chunkSize = 1024 * 1024
+    const size = chunkSize * 10
+    const input: Uint8Array[] = []
+
+    const candidate: FileCandidate = {
+      content: (async function * () {
+        for (let i = 0; i < size; i += chunkSize) {
+          const buf = new Uint8Array(chunkSize)
+          input.push(buf)
+
+          yield buf
+        }
+      }())
+    }
+
+    const { cid } = await kubo.api.add(candidate.content)
+    const bytes = await toBuffer(kubo.api.dag.export(cid))
+
+    const reader = await CarReader.fromBytes(bytes)
+
+    await c.import(reader)
+
+    expect(await toBuffer(u.cat(cid))).to.equalBytes(toBuffer(input))
+  })
+})
