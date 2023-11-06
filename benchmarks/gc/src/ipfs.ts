40aa50225870e27301e9a12396da3e95ce5e6d61
Isaac Emmanuel <35623334+1cbyc@users.noreply.github.com>
2023-11-06 23:56:00 +0100

    see ipfs.ts
    used "try...catch" block to handle potential errors and log them to the console for debugging. 
    
    adjusted the import functions too.

--- benchmarks/gc/src/ipfs.ts
+++ benchmarks/gc/src/ipfs.ts
@@ -1,68 +1,99 @@
-import os from 'node:os'
-import path from 'node:path'
-import { create } from 'ipfs-core'
-import all from 'it-all'
-import drain from 'it-drain'
-import type { GcBenchmark } from './index.js'
+const os = require('os');
+const path = require('path');
+const { create } = require('ipfs-core');
+const all = require('it-all');
+const drain = require('it-drain');
+const { GcBenchmark } = require('./index.js');
 
-export async function createIpfsBenchmark (): Promise<GcBenchmark> {
-  const repoPath = path.join(os.tmpdir(), `ipfs-${Math.random()}`)
+export async function createIpfsBenchmark () {
+  try {
+    const repoPath = path.join(os.tmpdir(), `ipfs-${Math.random()}`);
 
-  const ipfs = await create({
-    config: {
-      Addresses: {
-        Swarm: []
-      }
-    },
-    repo: repoPath,
-    start: false,
-    init: {
-      emptyRepo: true
-    }
-  })
-
-  return {
-    async gc () {
-      await drain(ipfs.repo.gc())
-    },
-    async putBlocks (blocks) {
-      for (const { value } of blocks) {
-        await ipfs.block.put(value)
-      }
-    },
-    async pin (cid) {
-      await ipfs.pin.add(cid)
-    },
-    async teardown () {
-      await ipfs.stop()
-    },
-    async clearPins () {
-      const pins = await all(ipfs.pin.ls())
-
-      for (const pin of pins) {
-        if (pin.type !== 'recursive' && pin.type !== 'direct') {
-          continue
+    const ipfs = await create({
+      config: {
+        Addresses: {
+          Swarm: []
         }
-
-        await ipfs.pin.rm(pin.cid)
+      },
+      repo: repoPath,
+      start: false,
+      init: {
+        emptyRepo: true
       }
+    });
 
-      return pins.length
-    },
-    isPinned: async (cid) => {
-      const result = await all(ipfs.pin.ls({
-        paths: cid
-      }))
+    return {
+      async gc () {
+        try {
+          await drain(ipfs.repo.gc());
+        } catch (error) {
+          console.error('Error in gc:', error);
+        }
+      },
+      async putBlocks (blocks) {
+        for (const { value } of blocks) {
+          try {
+            await ipfs.block.put(value);
+          } catch (error) {
+            console.error('Error in putBlocks:', error);
+          }
+        }
+      },
+      async pin (cid) {
+        try {
+          await ipfs.pin.add(cid);
+        } catch (error) {
+          console.error('Error in pin:', error);
+        }
+      },
+      async teardown () {
+        try {
+          await ipfs.stop();
+        } catch (error) {
+          console.error('Error in teardown:', error);
+        }
+      },
+      async clearPins () {
+        try {
+          const pins = await all(ipfs.pin.ls());
 
-      return result[0].type.includes('direct') || result[0].type.includes('indirect') || result[0].type.includes('recursive')
-    },
-    hasBlock: async (cid) => {
-      try {
-        await ipfs.block.get(cid)
-        return true
-      } catch {
-        return false
+          for (const pin of pins) {
+            if (pin.type !== 'recursive' && pin.type !== 'direct') {
+              continue;
+            }
+
+            await ipfs.pin.rm(pin.cid);
+          }
+
+          return pins.length;
+        } catch (error) {
+          console.error('Error in clearPins:', error);
+        }
+      },
+      isPinned: async (cid) => {
+        try {
+          const result = await all(ipfs.pin.ls({
+            paths: cid
+          }));
+
+          return result[0].type.includes('direct') || result[0].type.includes('indirect') || result[0].type.includes('recursive');
+        } catch (error) {
+          console.error('Error in isPinned:', error);
+          return false; // Return false if an error occurs
+        }
+      },
+      hasBlock: async (cid) => {
+        try {
+          await ipfs.block.get(cid);
+          return true;
+        } catch (error) {
+          console.error('Error in hasBlock:', error);
+          return false;
+        }
       }
-    }
+    };
+  } catch (error) {
+    console.error('Error in createIpfsBenchmark:', error);
+    throw error; // Rethrow the error to handle it at a higher level
   }
 }
