5c4fd54207384165c4e6309ec7663e996d7d66d4
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-01-09 08:08:55 -0800

    fix: remove w3s.link default block-broker (#371)
    Note that w3s will only serve blocks that are uploaded to it

--- packages/block-brokers/src/trustless-gateway/index.ts
+++ packages/block-brokers/src/trustless-gateway/index.ts
@@ -11,10 +11,7 @@ export const DEFAULT_TRUSTLESS_GATEWAYS = [
   'https://cf-ipfs.com',
 
   // 2023-10-03: IPNS, Origin, and Block/CAR support from https://ipfs-public-gateway-checker.on.fleek.co/
-  'https://4everland.io',
-
-  // 2023-10-03: IPNS, Origin, and Block/CAR support from https://ipfs-public-gateway-checker.on.fleek.co/
-  'https://w3s.link'
+  'https://4everland.io'
 ]
 
 export type TrustlessGatewayGetBlockProgressEvents =
