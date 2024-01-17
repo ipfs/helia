3477b2748d44a862e8afeae1a7a2668cdd8a7100
Alex Potsides <alex@achingbrain.net>
2024-01-17 08:36:58 +0100

    feat: export binary from @helia/interop (#384)
    To prevent things like transient deps breaking Kubo interop tests,
    export a bin to allow installing the interop tests from npm and
    running them in an easy-to-digest way.

--- packages/interop/src/index.ts
+++ packages/interop/src/index.ts
@@ -1,1 +1,14 @@
+/**
+ * @packageDocumentation
+ *
+ * Runs interop tests between Helia and Kubo.
+ *
+ * @example Testing a new Kubo release
+ *
+ * ```console
+ * $ npm i @helia/interop
+ * $ KUBO_BINARY=/path/to/kubo helia-interop
+ * ```
+ */
+
 export {}
