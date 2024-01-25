28a2dc4b0896a1e5052b4b680647d7070444505c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-01-25 10:50:05 -0800

    chore: add onProgress event types

--- packages/verified-fetch/src/interface.ts
+++ packages/verified-fetch/src/interface.ts
@@ -1,4 +1,8 @@
+import type { ResolveDnsLinkProgressEvents, ResolveProgressEvents } from '@helia/ipns'
+import type { GetEvents } from '@helia/unixfs'
+import type { AbortOptions } from '@libp2p/interface'
 import type { CID } from 'multiformats/cid'
+import type { ProgressEvent, ProgressOptions } from 'progress-events'
 
 /**
  * Instead of passing a Helia instance, you can pass a list of gateways and routers, and a HeliaHTTP instance will be created for you.
@@ -13,12 +17,31 @@ export interface CreateVerifiedFetchWithOptions {
  */
 export type ResourceType = string | CID
 
+export type BubbledProgressEvents =
+  // unixfs
+  GetEvents |
+  // ipns
+  ResolveProgressEvents | ResolveDnsLinkProgressEvents
+
+export interface CidDetail {
+  cid: string
+  path: string
+}
+
+export interface CidDetailError extends CidDetail {
+  error: Error
+}
+
+export type VerifiedFetchProgressEvents =
+  ProgressEvent<'verified-fetch:request:start', CidDetail> |
+  ProgressEvent<'verified-fetch:request:progress:chunk', CidDetail> |
+  ProgressEvent<'verified-fetch:request:end', CidDetail> |
+  ProgressEvent<'verified-fetch:request:error', CidDetailError>
+
 /**
  * The second argument of the `verifiedFetch` function.
  */
-export interface VerifiedFetchOptions extends RequestInit {
-  signal?: AbortSignal
-  // onProgress?: (progress: any) => void
+export interface VerifiedFetchOptions extends Omit<RequestInit, 'signal'>, AbortOptions, ProgressOptions<BubbledProgressEvents | VerifiedFetchProgressEvents> {
 }
 
 export interface ParsedUrlStringResults {
