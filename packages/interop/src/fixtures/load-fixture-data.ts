f864f441acbd3746b54bde508a7fa6fdd5784c59
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-01-26 11:20:57 -0800

    chore: replace fixture server with aegir/fixtures

--- packages/interop/src/fixtures/load-fixture-data.ts
+++ packages/interop/src/fixtures/load-fixture-data.ts
@@ -1,7 +1,9 @@
+import loadFixture from 'aegir/fixtures'
 import drain from 'it-drain'
-import { getFixtureDataAsyncIterable } from './get-fixture-data.js'
 import type { Controller } from 'ipfsd-ctl'
 
-export async function loadFixtureDataCar (controller: Controller<'go'>, path: string): Promise<void> {
-  await drain(controller.api.dag.import(getFixtureDataAsyncIterable(path)))
+export async function loadFixtureDataCar (controller: Controller, path: string): Promise<void> {
+  const fixtureData = `src/fixtures/data/${path}`
+  const buf = loadFixture(fixtureData)
+  await drain(controller.api.dag.import([buf]))
 }
