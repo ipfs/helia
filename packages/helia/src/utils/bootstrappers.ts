12a52bc20a5954000e070aed16a1295cc84e743a
Alex Potsides <alex@achingbrain.net>
2024-10-09 09:04:08 +0100

    chore: update bootstrappers.ts

--- packages/helia/src/utils/bootstrappers.ts
+++ packages/helia/src/utils/bootstrappers.ts
@@ -4,11 +4,9 @@ export const bootstrapConfig = {
     '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
     '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
     '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
-    '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
-
-    // va1 is not currently in the TXT records for
-    // `_dnsaddr.bootstrap.libp2p.io` so add the address manually as it would be
-    // after resolving the domain name
-    '/dns/va1.bootstrap.libp2p.io/tcp/443/tls/ws/p2p/12D3KooWKnDdG3iXw9eTFijk3EWSunZcFi54Zka4wmtqtt6rPxc8'
+    // va1 is not in the TXT records for _dnsaddr.bootstrap.libp2p.io yet
+    // so use the host name directly
+    '/dnsaddr/va1.bootstrap.libp2p.io/p2p/12D3KooWKnDdG3iXw9eTFijk3EWSunZcFi54Zka4wmtqtt6rPxc8',
+    '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ'
   ]
 }
