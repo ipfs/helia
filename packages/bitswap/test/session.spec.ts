87e7e74f2e30aa960a0fb8313330e52ef644d48b
achingbrain <alex@achingbrain.net>
2025-04-17 13:04:31 +0100

    feat: pass initial providers to session
    For when you know that certain peers have some or all of the blocks
    you want, allow passing providers to session implemenations to start
    from instead of always querying the routing/connected peers.
    
    ```js
    const buf = await helia.blocks.get(cid, {
      providers: [
        peerId1,
        peerId2
      ]
    })
    ```
    
    Session implementations accept an array of `PeerId | Multiaddr | Multiaddr[]`
    and use them if they can or discard them if they can't.

--- packages/bitswap/test/session.spec.ts
+++ packages/bitswap/test/session.spec.ts
@@ -13,12 +13,13 @@ import { stubInterface, type StubbedInstance } from 'sinon-ts'
 import { createBitswapSession } from '../src/session.js'
 import type { Network } from '../src/network.js'
 import type { WantList } from '../src/want-list.js'
-import type { ComponentLogger } from '@libp2p/interface'
+import type { ComponentLogger, Libp2p } from '@libp2p/interface'
 
 interface StubbedBitswapSessionComponents {
   network: StubbedInstance<Network>
   wantList: StubbedInstance<WantList>
   logger: ComponentLogger
+  libp2p: StubbedInstance<Libp2p>
 }
 
 describe('session', () => {
@@ -35,7 +36,8 @@ describe('session', () => {
       wantList: stubInterface<WantList>({
         peers: new PeerMap()
       }),
-      logger: defaultLogger()
+      logger: defaultLogger(),
+      libp2p: stubInterface<Libp2p>()
     }
   })
 
--- packages/interop/src/helia-blockstore-sessions.spec.ts
+++ packages/interop/src/helia-blockstore-sessions.spec.ts
@@ -2,16 +2,13 @@
 
 import { multiaddr } from '@multiformats/multiaddr'
 import { expect } from 'aegir/chai'
-import toBuffer from 'it-to-buffer'
 import { CID } from 'multiformats/cid'
-import * as raw from 'multiformats/codecs/raw'
-import { sha256 } from 'multiformats/hashes/sha2'
 import { createHeliaNode } from './fixtures/create-helia.js'
 import { createKuboNode } from './fixtures/create-kubo.js'
 import type { HeliaLibp2p } from 'helia'
 import type { KuboInfo, KuboNode } from 'ipfsd-ctl'
 
-describe('helia - blockstore', () => {
+describe('helia - blockstore sessions', () => {
   let helia: HeliaLibp2p
   let kubo: KuboNode
   let kuboInfo: KuboInfo
@@ -20,9 +17,6 @@ describe('helia - blockstore', () => {
     helia = await createHeliaNode()
     kubo = await createKuboNode()
     kuboInfo = await kubo.info()
-
-    // connect the two nodes
-    await helia.libp2p.dial(kuboInfo.multiaddrs.map(str => multiaddr(str)))
   })
 
   afterEach(async () => {
@@ -35,23 +29,35 @@ describe('helia - blockstore', () => {
     }
   })
 
-  it('should be able to send a block', async () => {
-    const input = Uint8Array.from([0, 1, 2, 3, 4])
-    const digest = await sha256.digest(input)
-    const cid = CID.createV1(raw.code, digest)
-    await helia.blockstore.put(cid, input)
-    const output = await toBuffer(kubo.api.cat(cid))
-
-    expect(output).to.equalBytes(input)
-  })
-
-  it('should be able to receive a block', async () => {
+  it('should be able to receive a block from a peer', async () => {
     const input = Uint8Array.from([0, 1, 2, 3, 4])
     const { cid } = await kubo.api.add({ content: input }, {
       cidVersion: 1,
       rawLeaves: true
     })
+
+    await helia.libp2p.dial(kuboInfo.multiaddrs.map(str => multiaddr(str)))
+
     const output = await helia.blockstore.get(CID.parse(cid.toString()))
+
+    expect(output).to.equalBytes(input)
+  })
+
+  it('should be able to receive a block from a session provider', async () => {
+    const input = Uint8Array.from([0, 1, 2, 3, 4])
+    const { cid } = await kubo.api.add({ content: input }, {
+      cidVersion: 1,
+      rawLeaves: true
+    })
+    const root = CID.parse(cid.toString())
+
+    const session = helia.blockstore.createSession(root, {
+      providers: [
+        kuboInfo.multiaddrs.map(str => multiaddr(str))
+      ]
+    })
+
+    const output = await session.get(root)
 
     expect(output).to.equalBytes(input)
   })
