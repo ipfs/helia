3d773698389deb70e1a0181eb81fb8b5992857b8
Alex Potsides <alex@achingbrain.net>
2025-05-13 08:34:03 +0100

    feat: pass initial providers to session (#777)
    For when you know that certain peers have some or all of the blocks
    you want, allow passing providers to session implemenations to start
    from instead of always querying the routing/connected peers.
    
    ```js
    const buf = await helia.blocks.get(cid, {
      providers: [
        peerId1,
        peerId2
      ]
    })
    ```
    
    Session implementations accept an array of `PeerId | Multiaddr | Multiaddr[]`
    and use them if they can or discard them if they can't.

--- packages/block-brokers/src/trustless-gateway/session.ts
+++ packages/block-brokers/src/trustless-gateway/session.ts
@@ -1,11 +1,15 @@
 import { AbstractSession } from '@helia/utils'
-import { findHttpGatewayProviders } from './utils.js'
+import { isPeerId } from '@libp2p/interface'
+import { multiaddrToUri } from '@multiformats/multiaddr-to-uri'
+import { TrustlessGateway } from './trustless-gateway.js'
+import { filterNonHTTPMultiaddrs, findHttpGatewayProviders } from './utils.js'
 import { DEFAULT_ALLOW_INSECURE, DEFAULT_ALLOW_LOCAL } from './index.js'
 import type { CreateTrustlessGatewaySessionOptions } from './broker.js'
 import type { TrustlessGatewayGetBlockProgressEvents } from './index.js'
-import type { TransformRequestInit, TrustlessGateway } from './trustless-gateway.js'
+import type { TransformRequestInit } from './trustless-gateway.js'
 import type { BlockRetrievalOptions, Routing } from '@helia/interface'
-import type { ComponentLogger } from '@libp2p/interface'
+import type { ComponentLogger, PeerId } from '@libp2p/interface'
+import type { Multiaddr } from '@multiformats/multiaddr'
 import type { AbortOptions } from 'interface-store'
 import type { CID } from 'multiformats/cid'
 
@@ -53,6 +57,29 @@ class TrustlessGatewaySession extends AbstractSession<TrustlessGateway, Trustles
 
   equals (providerA: TrustlessGateway, providerB: TrustlessGateway): boolean {
     return providerA.url.toString() === providerB.url.toString()
+  }
+
+  async convertToProvider (provider: PeerId | Multiaddr | Multiaddr[], options?: AbortOptions): Promise<TrustlessGateway | undefined> {
+    if (isPeerId(provider)) {
+      return
+    }
+
+    const httpAddresses = filterNonHTTPMultiaddrs(Array.isArray(provider) ? provider : [provider], this.allowInsecure, this.allowLocal)
+
+    if (httpAddresses.length === 0) {
+      return
+    }
+
+    // take first address?
+    // /ip4/x.x.x.x/tcp/31337/http
+    // /ip4/x.x.x.x/tcp/31337/https
+    // etc
+    const uri = multiaddrToUri(httpAddresses[0])
+
+    return new TrustlessGateway(uri, {
+      logger: this.logger,
+      transformRequestInit: this.transformRequestInit
+    })
   }
 }
 
