0b5e50430a1a14d944e8c3666a638f36aa4f739c
achingbrain <alex@achingbrain.net>
2025-07-31 12:34:27 +0200

    feat: expose extended option for ls in mfs and unixfs
    Allows returning lightweight directory contents that does not require
    loading the root block of each directory entry.

--- packages/unixfs/src/commands/ls.ts
+++ packages/unixfs/src/commands/ls.ts
@@ -4,7 +4,7 @@ import { NoContentError, NotADirectoryError } from '../errors.js'
 import { resolve } from './utils/resolve.js'
 import type { LsOptions } from '../index.js'
 import type { GetStore } from '../unixfs.js'
-import type { UnixFSEntry } from 'ipfs-unixfs-exporter'
+import type { UnixFSEntry, UnixFSBasicEntry } from 'ipfs-unixfs-exporter'
 import type { CID } from 'multiformats/cid'
 
 const mergeOptions = mergeOpts.bind({ ignoreUndefined: true })
@@ -13,13 +13,29 @@ const defaultOptions: LsOptions = {
 
 }
 
-export async function * ls (cid: CID, blockstore: GetStore, options: Partial<LsOptions> = {}): AsyncIterable<UnixFSEntry> {
+export function ls (cid: CID, blockstore: GetStore, options: Partial<LsOptions & { extended: false }>): AsyncIterable<UnixFSBasicEntry>
+export function ls (cid: CID, blockstore: GetStore, options?: Partial<LsOptions>): AsyncIterable<UnixFSEntry>
+export async function * ls (cid: CID, blockstore: GetStore, options: Partial<LsOptions> = {}): AsyncIterable<any> {
   const opts: LsOptions = mergeOptions(defaultOptions, options)
   const resolved = await resolve(cid, opts.path, blockstore, opts)
-  const result = await exporter(resolved.cid, blockstore)
+  const result = await exporter(resolved.cid, blockstore, {
+    ...options,
+    extended: true
+  })
 
   if (result.type === 'file' || result.type === 'raw') {
-    yield result
+    if (options.extended === false) {
+      const basic: UnixFSBasicEntry = {
+        name: result.name,
+        path: result.path,
+        cid: result.cid
+      }
+
+      yield basic
+    } else {
+      yield result
+    }
+
     return
   }
 
@@ -31,8 +47,5 @@ export async function * ls (cid: CID, blockstore: GetStore, options: Partial<LsO
     throw new NotADirectoryError()
   }
 
-  yield * result.content({
-    offset: options.offset,
-    length: options.length
-  })
+  yield * result.content(options)
 }
