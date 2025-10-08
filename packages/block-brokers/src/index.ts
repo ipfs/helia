e605c6fce090c23a1554c122ef6600dc35339891
Dhruv Varshney <dhruvvarshney77@gmail.com>
2025-10-08 18:17:09 +0530

    docs: Example for customizing fetch requests to trustless gateways (#768)
    Add example code to the `packages/block-brokers/README.md` demonstrating how to customize the fetch requests sent to trustless gateways using the `transformRequestInit` option.This shows how to add authentication headers (such as JWT tokens) when using the `TrustlessGatewayBlockBroker`.
    
    Closes #757
    
    ---------
    
    Co-authored-by: Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
    Co-authored-by: Alex Potsides <alex@achingbrain.net>

--- packages/block-brokers/src/index.ts
+++ packages/block-brokers/src/index.ts
@@ -1,3 +1,49 @@
+/**
+ * @packageDocumentation
+ *
+ * ## Trustless Gateway Block Broker
+ *
+ * The TrustlessGatewayBlockBroker allows customizing fetch requests to HTTP gateways.
+ *
+ * @example Customizing fetch requests with custom headers
+ *
+ * ```typescript
+ * import { createHelia } from 'helia'
+ * import { trustlessGateway } from '@helia/block-brokers'
+ * import { httpGatewayRouting } from '@helia/routers'
+ * import { unixfs } from '@helia/unixfs'
+ * import { CID } from 'multiformats/cid'
+ * import { concat } from 'uint8arrays/concat'
+ * import all from 'it-all'
+ *
+ * const routing = httpGatewayRouting({
+ *   gateways: ['https://ipfs.io', 'https://dweb.link']
+ * })
+ *
+ * const helia = await createHelia({
+ *   routers: [routing],
+ *   blockBrokers: [
+ *     trustlessGateway({
+ *       transformRequestInit: (requestInit) => {
+ *         requestInit.headers = {
+ *           ...requestInit.headers,
+ *           'User-Agent': 'Helia Example Script'
+ *         }
+ *         return requestInit
+ *       }
+ *     })
+ *   ]
+ * })
+ *
+ * const fs = unixfs(helia)
+ * const cid = CID.parse('bafkreife2klsil6kaxqhvmhgldpsvk5yutzm4i5bgjoq6fydefwtihnesa')
+ * const chunks = await all(fs.cat(cid))
+ * const content = concat(chunks)
+ *
+ *
+ * ```
+ */
+
 export { bitswap } from './bitswap.js'
 export type { BitswapBlockBrokerInit, BitswapBlockBrokerComponents } from './bitswap.js'
 export { trustlessGateway } from './trustless-gateway/index.js'
