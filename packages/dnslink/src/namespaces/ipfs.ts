c01d2e1d232db84669de84b128560318bf40b040
achingbrain <alex@achingbrain.net>
2025-10-14 14:15:40 +0300

    feat: add lookup cache
    Adds cache to ensure we don't look up DNS records we've seen
    previously that are still within their TTL.

--- packages/dnslink/src/namespaces/ipfs.ts
+++ packages/dnslink/src/namespaces/ipfs.ts
@@ -1,22 +1,20 @@
 import { CID } from 'multiformats/cid'
 import { InvalidNamespaceError } from '../errors.ts'
-import type { DNSLinkResult, DNSLinkNamespace } from '../index.js'
+import type { DNSLinkParser, DNSLinkIPFSResult } from '../index.js'
 import type { Answer } from '@multiformats/dns'
 
-export const ipfs: DNSLinkNamespace = {
-  parse: (value: string, answer: Answer): DNSLinkResult => {
-    const [, protocol, cid, ...rest] = value.split('/')
+export const ipfs: DNSLinkParser<DNSLinkIPFSResult> = (value: string, answer: Answer): DNSLinkIPFSResult => {
+  const [, protocol, cid, ...rest] = value.split('/')
 
-    if (protocol !== 'ipfs') {
-      throw new InvalidNamespaceError(`Namespace ${protocol} was not "ipfs"`)
-    }
+  if (protocol !== 'ipfs') {
+    throw new InvalidNamespaceError(`Namespace ${protocol} was not "ipfs"`)
+  }
 
-    // if the result is a CID, we've reached the end of the recursion
-    return {
-      namespace: 'ipfs',
-      cid: CID.parse(cid),
-      path: rest.length > 0 ? `/${rest.join('/')}` : '',
-      answer
-    }
+  // if the result is a CID, we've reached the end of the recursion
+  return {
+    namespace: 'ipfs',
+    cid: CID.parse(cid),
+    path: rest.length > 0 ? `/${rest.join('/')}` : '',
+    answer
   }
 }
