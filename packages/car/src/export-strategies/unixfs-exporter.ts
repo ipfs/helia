921696f914fbe2af6ba04464fbbcf5072cf8d088
Alex Potsides <alex@achingbrain.net>
2025-10-19 17:57:32 +0200

    feat: add offset/length to export slices of DAGs (#881)
    Supports including only the blocks that contain requested bytes and
    is smart enough not to load blocks outside of that range.

--- packages/car/src/export-strategies/unixfs-exporter.ts
+++ packages/car/src/export-strategies/unixfs-exporter.ts
@@ -1,12 +1,53 @@
 import { depthFirstWalker } from '@helia/utils'
+import { setMaxListeners } from '@libp2p/interface'
+import { anySignal } from 'any-signal'
+import { UnixFS } from 'ipfs-unixfs'
 import { DAG_PB_CODEC_CODE, RAW_PB_CODEC_CODE } from '../constants.ts'
 import { NotUnixFSError } from '../errors.ts'
 import type { ExportStrategy } from '../index.js'
 import type { CodecLoader } from '@helia/interface'
+import type { PBNode } from '@ipld/dag-pb'
 import type { AbortOptions } from '@libp2p/interface'
 import type { Blockstore } from 'interface-blockstore'
 import type { BlockView } from 'multiformats'
-import type { CID } from 'multiformats/cid'
+import type { CID, Version } from 'multiformats/cid'
+
+export interface UnixFSExporterOptions {
+  /**
+   * Include blocks that start with this offset.
+   *
+   * Cannot be used with directories.
+   */
+  offset?: number
+
+  /**
+   * Only include blocks that would include this many file bytes (exclusive,
+   * inclusive of offset).
+   *
+   * Cannot be used with directories.
+   */
+  length?: number
+}
+
+function isRawBlock (block: BlockView<any, number, number, Version>): block is BlockView<Uint8Array, 0x55, number, 1> {
+  return block.cid.code === RAW_PB_CODEC_CODE
+}
+
+function isDagPBBlock (block: BlockView<any, number, number, Version>): block is BlockView<PBNode, 0x70, number, 1> {
+  return block.cid.code === DAG_PB_CODEC_CODE
+}
+
+function isFile (block: BlockView<any, number, number, 0 | 1>): boolean {
+  if (isRawBlock(block)) {
+    return true
+  } else if (isDagPBBlock(block) && block.value.Data != null) {
+    const u = UnixFS.unmarshal(block.value.Data)
+
+    return u.type === 'file' || u.type === 'raw'
+  } else {
+    throw new NotUnixFSError('Encountered non raw/dag-pb CID in UnixFS DAG')
+  }
+}
 
 /**
  * Traverses the DAG depth-first starting at the target CID and yields all
@@ -16,6 +57,12 @@ import type { CID } from 'multiformats/cid'
  * the helia config.
  */
 export class UnixFSExporter implements ExportStrategy {
+  private options?: UnixFSExporterOptions
+
+  constructor (options?: UnixFSExporterOptions) {
+    this.options = options
+  }
+
   async * export (cid: CID, blockstore: Blockstore, getCodec: CodecLoader, options?: AbortOptions): AsyncGenerator<BlockView<unknown, number, number, 0 | 1>, void, undefined> {
     if (cid.code !== DAG_PB_CODEC_CODE && cid.code !== RAW_PB_CODEC_CODE) {
       throw new NotUnixFSError('Target CID was not UnixFS - use the SubGraphExporter to export arbitrary graphs')
@@ -26,8 +73,62 @@ export class UnixFSExporter implements ExportStrategy {
       getCodec
     })
 
-    for await (const node of walker.walk(cid, options)) {
-      yield node.block
+    const offset = this.options?.offset ?? 0
+    const end = offset + (this.options?.length ?? Infinity)
+
+    let exportingFile: boolean
+    const abortController = new AbortController()
+    const signal = anySignal([
+      abortController.signal,
+      options?.signal
+    ])
+    setMaxListeners(Infinity, abortController.signal, signal)
+
+    function includeChild (child: CID, parent: BlockView<PBNode>): boolean {
+      if (exportingFile == null) {
+        exportingFile = isFile(parent)
+      }
+
+      // ignore offset/length if not exporting a file
+      if (!exportingFile) {
+        return true
+      }
+
+      const childIndex = parent.value.Links.findIndex(link => link.Hash.equals(child))
+      const layout = UnixFS.unmarshal(parent.value.Data ?? new Uint8Array())
+
+      const childStart = Number([...layout.blockSizes].slice(0, childIndex).reduce((curr, acc) => curr + acc, 0n))
+      const childEnd = childStart + Number(layout.blockSizes[childIndex])
+
+      // slice starts in child
+      if (offset >= childStart && offset < childEnd) {
+        return true
+      }
+
+      // slice ends in child
+      if (end >= childStart && end < childEnd) {
+        return true
+      }
+
+      // slice contains child
+      if (offset <= childStart && end >= childEnd) {
+        return true
+      }
+
+      return false
+    }
+
+    try {
+      for await (const node of walker.walk(cid, {
+        ...options,
+        includeChild,
+        signal
+      })) {
+        yield node.block
+      }
+    } finally {
+      abortController.abort()
+      signal.clear()
     }
   }
 }
