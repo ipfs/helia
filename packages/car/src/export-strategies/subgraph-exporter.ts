4bc335f0f2387c316c8377cf51f619ea0d86457f
achingbrain <alex@achingbrain.net>
2025-11-20 08:50:32 +0200

    feat: allow specifying graph export strategy
    Support BFS or DFS graph traversal as an option.

--- packages/car/src/export-strategies/subgraph-exporter.ts
+++ packages/car/src/export-strategies/subgraph-exporter.ts
@@ -1,10 +1,22 @@
-import { breadthFirstWalker } from '@helia/utils'
+import { breadthFirstWalker, depthFirstWalker } from '@helia/utils'
 import type { ExportStrategy } from '../index.js'
 import type { CodecLoader } from '@helia/interface'
+import type { GraphWalker } from '@helia/utils'
 import type { AbortOptions } from '@libp2p/interface'
 import type { Blockstore } from 'interface-blockstore'
 import type { BlockView } from 'multiformats'
 import type { CID } from 'multiformats/cid'
+
+export type SubgraphExporterOrder = 'breadth-first' | 'depth-first'
+
+export interface SubgraphExporterInit {
+  /**
+   * Graph traversal strategy
+   *
+   * @default 'breadth-first'
+   */
+  order?: 'breadth-first' | 'depth-first'
+}
 
 /**
  * Traverses the DAG breadth-first starting at the target CID and yields all
@@ -14,11 +26,26 @@ import type { CID } from 'multiformats/cid'
  * the helia config.
  */
 export class SubgraphExporter implements ExportStrategy {
+  private order: SubgraphExporterOrder
+
+  constructor (init?: SubgraphExporterInit) {
+    this.order = init?.order ?? 'breadth-first'
+  }
+
   async * export (cid: CID, blockstore: Blockstore, getCodec: CodecLoader, options?: AbortOptions): AsyncGenerator<BlockView<unknown, number, number, 0 | 1>, void, undefined> {
-    const walker = breadthFirstWalker({
-      blockstore,
-      getCodec
-    })
+    let walker: GraphWalker
+
+    if (this.order === 'depth-first') {
+      walker = depthFirstWalker({
+        blockstore,
+        getCodec
+      })
+    } else {
+      walker = breadthFirstWalker({
+        blockstore,
+        getCodec
+      })
+    }
 
     for await (const node of walker.walk(cid, options)) {
       yield node.block
