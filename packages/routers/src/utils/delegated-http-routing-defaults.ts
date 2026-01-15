f33ed1a40e90cc67c84fec9faa1aeea2086c9266
achingbrain <alex@achingbrain.net>
2026-01-15 18:36:09 +0100

    fix: use non-deprecated factory function to create delegated client
    The `create...` factory is going away so switch to the non-deprecated
    method.

--- packages/routers/src/utils/delegated-http-routing-defaults.ts
+++ packages/routers/src/utils/delegated-http-routing-defaults.ts
@@ -1,8 +1,10 @@
 import type { DelegatedRoutingV1HttpApiClientInit } from '@helia/delegated-routing-v1-http-api-client'
 
-export function delegatedHTTPRoutingDefaults (): DelegatedRoutingV1HttpApiClientInit {
+export function delegatedHTTPRoutingDefaults (init?: DelegatedRoutingV1HttpApiClientInit): DelegatedRoutingV1HttpApiClientInit & { url: string } {
   return {
+    url: 'https://delegated-ipfs.dev',
     filterProtocols: ['unknown', 'transport-bitswap', 'transport-ipfs-gateway-http'],
-    filterAddrs: ['https', 'tcp', 'webrtc', 'webrtc-direct', 'wss', 'tls']
+    filterAddrs: ['https', 'tcp', 'webrtc', 'webrtc-direct', 'wss', 'tls'],
+    ...(init ?? {})
   }
 }
