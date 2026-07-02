e488bd17d755d108378d00ecbc6b6725490ff183
Alex Potsides <alex@achingbrain.net>
2026-07-02 11:17:18 +0200

    fix: init added block brokers and routers (#1068)
    If a block broker or router is added by a mixin as a factory, ensure
    it is inited properly before use.

--- packages/helia/test/index.spec.ts
+++ packages/helia/test/index.spec.ts
@@ -1,4 +1,9 @@
 import { expect } from 'aegir/chai'
+import drain from 'it-drain'
+import toBuffer from 'it-to-buffer'
+import { CID } from 'multiformats/cid'
+import { sha256 } from 'multiformats/hashes/sha2'
+import { stubInterface } from 'sinon-ts'
 import { createHelia } from '../src/index.ts'
 import type { Helia } from '@helia/interface'
 
@@ -29,5 +34,79 @@ describe('helia', () => {
 
   it('should have a datastore', async () => {
     expect(helia).to.have.property('datastore').that.is.ok()
+  })
+
+  it('should add a block broker', async () => {
+    const block = crypto.getRandomValues(new Uint8Array(32))
+    const cid = CID.createV0(await sha256.digest(block))
+
+    await expect(drain(helia.blockstore.get(cid, {
+      signal: AbortSignal.timeout(100)
+    }))).to.eventually.be.rejected()
+
+    helia.addBlockBroker({
+      name: 'test-broker',
+      retrieve: async () => {
+        return block
+      }
+    })
+
+    await expect(toBuffer(helia.blockstore.get(cid)))
+      .to.eventually.deep.equal(block)
+  })
+
+  it('should add a block broker as a factory', async () => {
+    const block = crypto.getRandomValues(new Uint8Array(32))
+    const cid = CID.createV0(await sha256.digest(block))
+
+    await expect(drain(helia.blockstore.get(cid, {
+      signal: AbortSignal.timeout(100)
+    }))).to.eventually.be.rejected()
+
+    helia.addBlockBroker(() => ({
+      name: 'test-broker',
+      retrieve: async () => {
+        return block
+      }
+    }))
+
+    await expect(toBuffer(helia.blockstore.get(cid)))
+      .to.eventually.deep.equal(block)
+  })
+
+  it.skip('should add a router', async () => {
+    const block = crypto.getRandomValues(new Uint8Array(32))
+    const cid = CID.createV0(await sha256.digest(block))
+
+    const router = stubInterface({
+      name: 'test-broker',
+      findProviders: async function * () {}
+    })
+
+    helia.addRouter(router)
+
+    await expect(drain(helia.blockstore.get(cid, {
+      signal: AbortSignal.timeout(1_000)
+    }))).to.eventually.be.rejected()
+
+    expect(router.findProviders.called).to.be.true()
+  })
+
+  it.skip('should add a router as a factory', async () => {
+    const block = crypto.getRandomValues(new Uint8Array(32))
+    const cid = CID.createV0(await sha256.digest(block))
+
+    const router = stubInterface({
+      name: 'test-broker',
+      findProviders: async function * () {}
+    })
+
+    helia.addRouter(() => router)
+
+    await expect(drain(helia.blockstore.get(cid, {
+      signal: AbortSignal.timeout(1_000)
+    }))).to.eventually.be.rejected()
+
+    expect(router.findProviders.called).to.be.true()
   })
 })
